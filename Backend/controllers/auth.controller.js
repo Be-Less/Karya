7be17a303b8f2a3787845b09648ba7d1aff59834
Bilesh <bileshlns@gmail.com>
2026-08-12 20:30:46 +0545

    feat: add user login and JWT authentication

--- Backend/controllers/auth.controller.js
+++ Backend/controllers/auth.controller.js
@@ -1,5 +1,6 @@
 import bcrypt from "bcryptjs";
 import User from "../models/user.model.js";
+import jwt from "jsonwebtoken";
 
 export const registerUser = async (req, res) => {
   try {
@@ -28,3 +29,34 @@ export const registerUser = async (req, res) => {
     res.status(500).json({ message: "Server error" });
   }
 };
+
+export const loginUser = async (req,res) => {
+  try{
+    const {email,password} = req.body;
+    const user = await User.findOne({email});
+    if(!user){
+      return res.status(400).json({message: "Invalid email or password"});
+    }
+    const isPasswordValid = await bcrypt.compare(password,user.password);
+    if(!isPasswordValid){
+      return res.status(400).json({message: "Invalid email or password"});
+    }
+    const token = jwt.sign(
+      {
+        userId: user._id
+      },
+      process.env.JWT_SECRET,
+      {expiresIn: "7d"}
+    );
+    res.status(200).json({
+      message: "Login successful",
+      token,
+    });
+  }
+  catch(error){
+    console.error("Error in loginUser:", error.message);
+    res.status(500).json({ message: "Server error" });
+  }
+}
+
+
