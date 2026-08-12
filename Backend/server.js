41b9ecea85aeac80ea1b374d590c64053ee0b27a
Bilesh <bileshlns@gmail.com>
2026-08-12 19:32:21 +0545

    Created userSchema

--- Backend/server.js
+++ Backend/server.js
@@ -1,12 +1,18 @@
 import express from "express";
+import dotenv from "dotenv";
+import connectDB from "./config/db.js";
 
+
+dotenv.config();
 const app = express();
 
 app.get("/", (req, res) => {
   res.send("Welcome to TaskForge😈");
 });
 
-const PORT = process.env.PORT || 3001;
+// Calling connectDB function to establish a connection to the MongoDB database
+connectDB();
+const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
   console.log(`Server is running on port http://localhost:${PORT}`);
 });
