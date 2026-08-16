504b0cc268ba5d08892ad677ec96e8a76610f064
Beless <bileshlns@gmail.com>
2026-08-16 17:45:27 +0545

    feat: add get project endpoint

--- Backend/controllers/project.controller.js
+++ Backend/controllers/project.controller.js
@@ -1,23 +1,24 @@
 import Project from "../models/project.model.js";
-
+import mongoose from "mongoose";
 export const createProject = async (req, res, next) => {
-  try {const { name, description } = req.body;
+  try {
+    const { name, description } = req.body;
 
-const project = await Project.create({
-  name,
-  description,
-  owner: req.user.userId,
-  members: [
-    {
-      user: req.user.userId,
-      role: "owner",
-    },
-  ],
-});
-res.status(201).json({
-  message: "Project created Successfully",
-  project,
-})
+    const project = await Project.create({
+      name,
+      description,
+      owner: req.user.userId,
+      members: [
+        {
+          user: req.user.userId,
+          role: "owner",
+        },
+      ],
+    });
+    res.status(201).json({
+      message: "Project created Successfully",
+      project,
+    });
   } catch (error) {
     next(errro);
   }
@@ -31,6 +32,34 @@ export const getProjects = async (req, res, next) => {
 
     res.status(200).json({
       projects,
+    });
+  } catch (error) {
+    next(error);
+  }
+};
+export const getProject = async (req, res, next) => {
+  try {
+    const { id } = req.params;
+
+    if (!mongoose.Types.ObjectId.isValid(id)) {
+      return res.status(400).json({
+        message: "Invalid project ID",
+      });
+    }
+
+    const project = await Project.findOne({
+      _id: id,
+      "members.user": req.user.userId,
+    });
+
+    if (!project) {
+      return res.status(404).json({
+        message: "Project not found",
+      });
+    }
+
+    res.status(200).json({
+      project,
     });
   } catch (error) {
     next(error);
--- Backend/routes/project.routes.js
+++ Backend/routes/project.routes.js
@@ -1,6 +1,7 @@
 import express from "express";
 import {
   createProject,
+  getProject,
   getProjects,
 } from "../controllers/project.controller.js";
 import { authMiddleware } from "../middleware/auth.middleware.js";
@@ -8,5 +9,6 @@ import { authMiddleware } from "../middleware/auth.middleware.js";
 const router = express.Router();
 router.post("/", authMiddleware, createProject);
 router.get("/", authMiddleware, getProjects);
+router.get("/:id", authMiddleware, getProject);
 
 export default router;
