f242d0a08e12b06f6ee3b864a91ab88887470dae
Be-Less <bileshlns@gmail.com>
2026-08-26 13:41:30 +0545

    chore: setup frontend routing

---
+++ Frontend/src/pages/Dashboard.tsx
@@ -0,0 +1,5 @@
+function Dashboard() {
+  return <h1>Karya Dashboard</h1>;
+}
+
+export default Dashboard;
---
+++ Frontend/src/pages/Register.tsx
@@ -0,0 +1,5 @@
+function Register() {
+  return <h1>Register for Karya</h1>;
+}
+
+export default Register;
